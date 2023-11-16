f9c93bf53fcc19044e80990a28d5eb3dff7dc591
Fan Yuan <fan_yuan@ymsl.com.cn>
2023-11-16 11:52:02 +0800

    update code after merge.

--- app/config/server.ts
+++ app/config/server.ts
@@ -13,8 +13,6 @@ declare global {
       OPENAI_ORG_ID?: string; // openai only
 
       VERCEL?: string;
-      BUILD_MODE?: "standalone" | "export";
-      BUILD_APP?: string; // is building desktop app
 
       HIDE_USER_API_KEY?: string; // disable user's api key input
       DISABLE_GPT4?: string; // allow user to use gpt-4 or not
