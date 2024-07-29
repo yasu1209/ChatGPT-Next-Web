1aa10c3f9ca1db4f4d5a272acf2e054bc86ac8c3
FanYuan0208 <fan_yuan@ymsl.com.cn>
2024-07-29 16:50:02 +0800

    support stability to auth

--- app/api/stability/[...path]/route.ts
+++ app/api/stability/[...path]/route.ts
@@ -39,7 +39,7 @@ async function handle(
     10 * 60 * 1000,
   );
 
-  const authResult = auth(req, ModelProvider.Stability);
+  const authResult = await auth(req, ModelProvider.Stability);
 
   if (authResult.error) {
     return NextResponse.json(authResult, {
